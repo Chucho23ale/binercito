2fa172a7df912664576722588b720bc24a7cbc20
Jesus Alejandro Morales Migueles <morales.j.a@outlook.com>
2023-10-16 17:17:52 -0600

    rosserial 2 llantas con twist

--- src/rosserial/rosserial.ino
+++ src/rosserial/rosserial.ino
@@ -1,61 +1,90 @@
 #include <stdlib.h>
 #include <ros.h>
+#include <geometry_msgs/Twist.h>
 #include <std_msgs/String.h>
 #include <std_msgs/Float32.h>
 
+ 
+
+#define WHEELRAD 0.0636 //The radius of the wheel (m) 
+
+#define WHEELDIST 0.34 //Distance between wheels (m) 
+
 
 ros::NodeHandle  nh;
-std_msgs::Float32 ref_msg;
-std_msgs::Float32 ret_msg;
-ros::Publisher referenciaT("referencia", &ref_msg);
-ros::Publisher retroT("retro", &ret_msg);
+std_msgs::Float32 wl_ref_msg, wl_msg, wr_ref_msg, wr_msg;
+geometry_msgs::Twist twist;
+ros::Publisher wl_refT("wl_ref", &wl_ref_msg);
+ros::Publisher wlT("wl", &wl_msg);
+ros::Publisher wr_refT("wr_ref", &wr_ref_msg);
+ros::Publisher wrT("wr", &wr_msg);
+
 
 
 //Encoder #1
-const int pinCanalA=2;
-const int pinCanalB=4;
+const int pinCanalAl=2;
+const int pinCanalBl=4;
+
+//Encoder #2
+const int pinCanalAr=3;
+const int pinCanalBr=5;
 
 //Control Motor 1
-const int motorPin1=10;
-const int motorPin2=11;
+const int motorPin1l=10;
+const int motorPin2l=11;
 
+//Control Motor 2
+const int motorPin1r=12;
+const int motorPin2r=13;
 
 float dt=0.02;
 float tiempo=0;
-float contador=0;
-float pos=0;
-float pos_ant=0;
-float vel=0;
-float VMotor=0;
-float valorM=0;
+float contadorl=0;
+float contadorr=0;
+float posl=0;
+float posr=0;
+float pos_antl=0;
+float pos_antr=0;
 
 
 float Ts;
 float kp, ki, kd;
 float q0, q1, q2;
-float u[2] = {0.0, 0.0}; // u[0] salida actual   u[1] salida anterior
-float e[3] = {0.0, 0.0, 0.0};  //e[0] error actual    e[1] anterior  e[2] dos veces anterior
-float referencia=0;
-float retro; //retroalimentacion
+float ul[2] = {0.0, 0.0}; // u[0] salida actual   u[1] salida anterior
+float el[3] = {0.0, 0.0, 0.0};  //e[0] error actual    e[1] anterior  e[2] dos veces anterior
+float ur[2] = {0.0, 0.0}; // u[0] salida actual   u[1] salida anterior
+float er[3] = {0.0, 0.0, 0.0};  //e[0] error actual    e[1] anterior  e[2] dos veces anterior
+float wl_ref=0;
+float wr_ref=0;
+float wl=0;
+float wr=0;
+float v = 0;
+float w = 0;
 
-
-void messageCb( const std_msgs::String& refstring){
-  String refs = refstring.data;
-  referencia = refs.toFloat();
+void messageCb( const geometry_msgs::Twist& tw_cb){
+  twist = tw_cb;
 }
 
-ros::Subscriber<std_msgs::String> sub("ref", &messageCb );
+ros::Subscriber<std_msgs::String> sub("ard_motor", &messageCb );
 
 
 //Configuracion de puertos E/S, Serial e interrupciones
 void setup() {
   Serial.begin(9600);
-  attachInterrupt(digitalPinToInterrupt(pinCanalA),Encoder,RISING);
-  pinMode(pinCanalA,INPUT);
-  pinMode(pinCanalB,INPUT);
+  attachInterrupt(digitalPinToInterrupt(pinCanalAl),Encoder,RISING);
+  pinMode(pinCanalAl,INPUT);
+  pinMode(pinCanalBl,INPUT);
 
-  pinMode(motorPin1,OUTPUT);
-  pinMode(motorPin2,OUTPUT);
+  
+  attachInterrupt(digitalPinToInterrupt(pinCanalAr),Encoder2,RISING);
+  pinMode(pinCanalAr,INPUT);
+  pinMode(pinCanalBr,INPUT);
+  
+  pinMode(motorPin1l,OUTPUT);
+  pinMode(motorPin2l,OUTPUT);
+
+  pinMode(motorPin1r,OUTPUT);
+  pinMode(motorPin2r,OUTPUT);
   
   kp = 4.4;
   ki = 30;
@@ -68,8 +97,10 @@ void setup() {
 
   nh.initNode();
   nh.subscribe(sub);
-  nh.advertise(referenciaT);
-  nh.advertise(retroT);
+  nh.advertise(wl_refT);
+  nh.advertise(wlT);
+  nh.advertise(wr_refT);
+  nh.advertise(wrT);
 
 }
 
@@ -77,41 +108,78 @@ void loop() {
 // Linea de tiempo se usa para controlar el tiempo demuestreo  
  if(micros()-tiempo>dt*1000000)
  {
+  v = twist.linear.x;
+  w = twist.angular.z;
+  wl_ref = (2.0 * v - WHEELDIST * w) / (2.0 * WHEELRAD);
+  wr_ref = (2.0 * v + WHEELDIST * w) / (2.0 * WHEELRAD);
+  
  tiempo=micros();
-  pos=contador;
+  posl=contadorl;
+  posr=contadorr;
   //Realizar la converción según el motor
-  vel=(pos-pos_ant)*2.6786;
+  wl=(posl-pos_antl)*2.6786;
+  wr=(posr-pos_antr)*2.6786;
 
 //INICIO DE AREA DE CONTROL
-    retro = vel;
     //calcular la señal de error
-    e[0] = referencia-retro;
+    el[0] = wl_ref-wl;
     //calcular la ecuación en diferencias
-    u[0]=q0*e[0]+q1*e[1]+q2*e[2]+u[1];
+    ul[0]=q0*el[0]+q1*el[1]+q2*el[2]+ul[1];
     //limitar la señal de control
-    if(u[0] > 255.0) u[0] = 255;
-    if(u[0] < -255.0) u[0] = -255;
+    if(ul[0] > 255.0) ul[0] = 255;
+    if(ul[0] < -255.0) ul[0] = -255;
     //corrimiento para mover las muestras
-    e[2] = e[1];
-    e[1] = e[0];
-    u[1] = u[0];
+    el[2] = el[1];
+    el[1] = el[0];
+    ul[1] = ul[0];
+// //FIN DE AREA DE CONTROL
+
+//INICIO DE AREA DE CONTROL
+    //calcular la señal de error
+    er[0] = wr_ref-wr;
+    //calcular la ecuación en diferencias
+    ur[0]=q0*er[0]+q1*er[1]+q2*er[2]+ur[1];
+    //limitar la señal de control
+    if(ur[0] > 255.0) ur[0] = 255;
+    if(ur[0] < -255.0) ur[0] = -255;
+    //corrimiento para mover las muestras
+    er[2] = er[1];
+    er[1] = er[0];
+    ur[1] = ur[0];
 // //FIN DE AREA DE CONTROL
 
   //Enviamos voltaje a puertos salida PWM
-  if ((int)u[0]<0){
-    analogWrite(motorPin1,0);
-    analogWrite(motorPin2,(int)(-u[0]));
+  if ((int)ul[0]<0){
+    analogWrite(motorPin1l,0);
+    analogWrite(motorPin2l,(int)(-ul[0]));
   }
-  if ((int)u[0]>=0){
-    analogWrite(motorPin1,(int)u[0]);
-    analogWrite(motorPin2,0);
+  if ((int)ul[0]>=0){
+    analogWrite(motorPin1l,(int)ul[0]);
+    analogWrite(motorPin2l,0);
   }
-  ref_msg.data = referencia;
-  ret_msg.data = retro;
-  referenciaT.publish(&ref_msg);
-  retroT.publish(&ret_msg);
+
+  //Enviamos voltaje a puertos salida PWM
+  if ((int)ur[0]<0){
+    analogWrite(motorPin1r,0);
+    analogWrite(motorPin2r,(int)(-ur[0]));
+  }
+  if ((int)ur[0]>=0){
+    analogWrite(motorPin1r,(int)ur[0]);
+    analogWrite(motorPin2r,0);
+  }
   
-  pos_ant=pos;
+  wl_ref_msg.data = wl_ref;
+  wl_msg.data = wl;
+  wl_refT.publish(&wl_ref_msg);
+  wlT.publish(&wl_msg);
+
+  wr_ref_msg.data = wr_ref;
+  wr_msg.data = wr;
+  wr_refT.publish(&wr_ref_msg);
+  wrT.publish(&wr_msg);
+  
+  pos_antl=posl;
+  pos_antr=posr;
  }
  nh.spinOnce();
  delay(1);
@@ -120,8 +188,17 @@ void loop() {
 //Interrupcion de Encoder 1
 void Encoder()
 {
-  if (digitalRead(pinCanalA)==digitalRead(pinCanalB)){
-    contador--;}
+  if (digitalRead(pinCanalAl)==digitalRead(pinCanalBl)){
+    contadorl--;}
     else{
-      contador++;}
+      contadorl++;}
+}
+
+//Interrupcion de Encoder 2
+void Encoder2()
+{
+  if (digitalRead(pinCanalAr)==digitalRead(pinCanalBr)){
+    contadorr--;}
+    else{
+      contadorr++;}
 }
